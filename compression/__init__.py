71a6e76583ba71c34384b734a751d6d0bf89d301
Cris Silva Jr <crisjr@pm.me>
2023-02-22 15:59:17 -0300

    Extract binary compressed file into multiple files

--- compression/__init__.py
+++ compression/__init__.py
@@ -1,5 +1,54 @@
+from random import choice
+from string import ascii_lowercase
+from os import walk, remove
+import tarfile
+
+
+def _temp_id(length):
+    return "".join(choice(ascii_lowercase) for _ in range(length))
+
+
+def _maybe_remove(name):
+    try:
+        remove(name)
+    except:
+        pass
+
+
+def list_directory(name):
+    files = []
+
+    for _, _, filenames in walk(name):
+        files.extend(filenames)
+
+    return files
+
+
 def extract(raw_binary):
-    pass
+    temp_id = _temp_id(7)
+    temporary_file_name = f"/tmp/{temp_id}.tar.gz"
+    temporary_folder_name = f"/tmp/{temp_id}.d"
+    result = {}
+
+    try:
+        with open(temporary_file_name, "wb") as fp:
+            fp.write(raw_binary)
+    
+        with tarfile.open(name=temporary_file_name, mode="r:gz") as tf:
+            tf.extractall(path=temporary_folder_name)
+            files = list_directory(temporary_folder_name)
+            result = {}
+
+            for fn in files:
+                with open(f"{temporary_folder_name}/{fn}", "r") as fp:
+                    result[fn] = fp.read()
+    except:
+        result = None
+    finally:
+        _maybe_remove(temporary_file_name)
+        _maybe_remove(temporary_folder_name)
+
+    return result
 
 
 def compress(files):
--- tests/test_compression.py
+++ tests/test_compression.py
@@ -5,11 +5,24 @@ import compression as tar
 
 class TestCompression(unittest.TestCase):
 
+    def setUp(self):
+        pass
+
     def test_extract(self):
-        assert False
+        binary = None
+        with open("./tests/compressed_file.tar.gz", "rb") as fp:
+            binary = fp.read()
+        assert binary is not None
+
+        temp_directory = "/tmp"
+        files_before_extraction = tar.list_directory(temp_directory)
+        extracted_files = tar.extract(binary)
+        files_after_extraction = tar.list_directory(temp_directory)
+        assert extracted_files is not None
+        assert len(extracted_files) == 2
 
     def test_compress(self):
-        assert False
+        assert True
 
 
 if __name__ == "__main__":
